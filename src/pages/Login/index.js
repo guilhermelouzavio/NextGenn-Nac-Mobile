adb7eda0da40dd6bdc09804451634fe33b65a106
Bruno T <bteixeira010@gmail.com>
2020-08-17 22:05:46 -0300

    logo

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
-import {SafeAreaView, Text, TextInput, TouchableOpacity} from 'react-native';
+import {Image, SafeAreaView, Text, TextInput, TouchableOpacity} from 'react-native';
 import styles from './styles';
+import logoNasaImg from '../../assets/logoNasa.png'
 
 const Login = () => {
   return (
+
     <SafeAreaView style={styles.container}>
+      <Image source={logoNasaImg}/>
       <TextInput style={styles.textoInput} placeholder={'Usuário'} />
       <TextInput
         style={styles.textoInput}
